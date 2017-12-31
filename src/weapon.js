331b96c8facd44a8ca00e96e5eb720184a0f3a4c
Sarcoma <sarcoma@live.co.uk>
2017-12-31 13:14:40 +0000

    Moved turn to weapon method + added extra variables

--- src/weapon.js
+++ src/weapon.js
@@ -11,6 +11,8 @@ export default class Weapon {
         this.lastFired = 0;
         this.pm = pm;
         this.rotation = 'right';
+        this.angleLimit = 0.4;
+        this.turningSpeed = 0.01;
     }
 
     setTarget(target) {
@@ -40,7 +42,25 @@ export default class Weapon {
         ctx.resetTransform();
     }
 
+    turn() {
+        switch(true) {
+            case this.rotation === 'right' && this.angle < this.angleLimit:
+                this.angle += this.turningSpeed;
+                break;
+            case this.rotation === 'left' && this.angle > -this.angleLimit:
+                this.angle -= this.turningSpeed;
+                break;
+            case this.rotation === 'right' && this.angle > this.angleLimit:
+                this.rotation = 'left';
+                break;
+            case this.rotation === 'left' && this.angle < -this.angleLimit:
+                this.rotation = 'right';
+                break;
+        }
+    }
+
     update() {
+        this.turn();
         if((deltaTime.getElapsedTime() - this.lastFired) > this.fireRate) {
             this.fire();
             this.lastFired = deltaTime.getElapsedTime();
