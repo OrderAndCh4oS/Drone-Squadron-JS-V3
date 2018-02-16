fd75aba8a295ef5c1975dc4879d74e083ee25354
Sarcoma <sarcoma@live.co.uk>
2018-02-16 09:53:17 +0000

    Add dodge if has close bullets angled toward drone

--- src/utility/steering.js
+++ src/utility/steering.js
@@ -6,23 +6,23 @@ export default class Steering {
         this.turnAmount = 0;
         this.roaming = {callback: null, count: 0};
         this.evading = {callback: null, count: 0};
+        this.dodging = {callback: null, count: 0};
     }
 
     turn(drone) {
         this.drone = drone;
-        if(!drone.scanner.hasTarget()) {
-            this.roam();
-            return;
-        }
-        if(drone.scanner.hasTarget() &&
-            distanceTo(drone, drone.scanner.target) < 100 &&
-            !angleBetweenRange(drone, Math.PI / 2)) {
-            this.evade();
-            return;
-        }
         const angleToTarget = angleTo(drone.angle,
             drone.scanner.angleToTarget());
         switch(true) {
+            case this.hasTargetCloseBehind(drone):
+                this.evade();
+                break;
+            case this.hasThreats(drone):
+                this.dodge();
+                break;
+            case this.hasNoTarget(drone):
+                this.roam();
+                break;
             case (angleToTarget >= 0.6):
                 this.turnAmount = this.turningSpeed * 0.1;
                 this.turnLeft();
@@ -58,6 +58,20 @@ export default class Steering {
         }
     }
 
+    hasTargetCloseBehind(drone) {
+        return (drone.scanner.hasTarget() &&
+            distanceTo(drone, drone.scanner.target) < 200 &&
+            !angleBetweenRange(drone, Math.PI / 2));
+    }
+
+    hasThreats(drone) {
+        return drone.scanner.threats > 0;
+    }
+
+    hasNoTarget(drone) {
+        return !drone.scanner.hasTarget();
+    }
+
     turnLeft() {
         this.incrementAngle(-this.turnAmount);
     }
@@ -88,8 +102,20 @@ export default class Steering {
             this.evading.callback();
             this.evading.count--;
         } else {
-            this.evading.count = Math.floor(Math.random() * 20 + 5);
+            this.evading.count = Math.floor(Math.random() * 35 + 15);
             this.evading.callback = Math.random() > 0.5 ? this.turnRight.bind(
+                this) : this.turnLeft.bind(this);
+        }
+    }
+
+    dodge() {
+        if(this.dodging.count > 0) {
+            this.turnAmount = this.turningSpeed * 0.06;
+            this.dodging.callback();
+            this.dodging.count--;
+        } else {
+            this.dodging.count = Math.floor(Math.random() * 10 + 5);
+            this.dodging.callback = Math.random() > 0.5 ? this.turnRight.bind(
                 this) : this.turnLeft.bind(this);
         }
     }
