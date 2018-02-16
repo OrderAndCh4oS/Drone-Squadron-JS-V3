454016ab1b03f5854b0a49c4dd40c77522fa1259
Sarcoma <sarcoma@live.co.uk>
2018-02-16 00:19:41 +0000

    Add explosion class, Add drone factory

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ import ParticleManager from './manager/particle-manager';
 import DroneManager from './manager/drone-manager';
 import GameGrid from './game-grid';
 import Debug from './dev/debug';
+import Background from './service/background';
 
 const canvas = document.getElementById('canvas');
 export const colours = {
@@ -17,6 +18,7 @@ export const context = canvas.getContext('2d');
 export const canvasWidth = canvas.width = window.innerWidth;
 export const canvasHeight = canvas.height = window.innerHeight;
 export const debug = new Debug();
+export const background = new Background();
 export const grid = new GameGrid();
 export const dm = new DroneManager(canvasWidth, canvasHeight);
 export const pm = new ParticleManager(canvasWidth, canvasHeight);
