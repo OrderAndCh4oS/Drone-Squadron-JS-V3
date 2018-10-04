5a1bca7005966b5492ff9647cccf0bafff3787dc
sarcoma <sarcoma@live.co.uk>
2018-10-04 12:25:40 +0100

    add index.js to music directory

---
+++ src/game/music/index.js
@@ -0,0 +1,17 @@
+import eightBitMayhem from "./8-Bit-Mayhem.mp3";
+import eightiesSpace from "./80s-Space.mp3";
+import automation from "./Automation.mp3";
+import coolAdventure from "./Cool-Adventure.mp3";
+import tapTillYouSnap from "./Tap-Till-You-Snap.mp3";
+import urbanFuture from "./Urban-Future.mp3";
+
+const music = [
+   eightBitMayhem,
+   eightiesSpace,
+   automation,
+   coolAdventure,
+   tapTillYouSnap,
+   urbanFuture,
+];
+
+export default music
