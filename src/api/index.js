d24cde40ad202c2b16384dea2c47525fe71c4600
sarcoma <sarcoma@live.co.uk>
2018-10-11 18:18:24 +0100

    Add login form

---
+++ src/api/index.js
@@ -0,0 +1,141 @@
+/* eslint-disable indent */
+
+const baseURL = 'http://localhost:5000';
+
+export const postRegister = ({values: {username, password}}) => {
+    return postFetch(makeUrl('/register'), {username, password});
+};
+
+export const postLogin = ({values: {username, password}}) => {
+    return postFetch(makeUrl('/login'), {username, password});
+};
+
+export const getUser = ({params}) => {
+    return getFetch(makeUrl('/user', params));
+};
+
+export const putUser = ({params}) => {
+    return putFetch(makeUrl('/user', params));
+};
+
+export const deleteUser = ({params}) => {
+    return deleteFetch(makeUrl('/user', params));
+};
+
+export const getSquadron = ({params}) => {
+    return getFetch(makeUrl('/squadron', params));
+};
+
+export const postSquadron = ({params}) => {
+    return postFetch(makeUrl('/squadron', params));
+};
+
+export const putSquadron = ({params}) => {
+    return putFetch(makeUrl('/squadron', params));
+};
+
+export const deleteSquadron = ({params}) => {
+    return deleteFetch(makeUrl('/squadron', params));
+};
+
+export const getDrone = ({params}) => {
+    return getFetch(makeUrl('/drone', params));
+};
+
+export const postDrone = ({params}) => {
+    return postFetch(makeUrl('/drone', params));
+};
+
+export const putDrone = ({params}) => {
+    return putFetch(makeUrl('/drone', params));
+};
+
+export const deleteDrone = ({params}) => {
+    return deleteFetch(makeUrl('/drone', params));
+};
+
+export const getPriceList = () => {
+    return getFetch(makeUrl('/price-list'));
+};
+
+export const getWeapon = () => {
+    return getFetch(makeUrl('/weapon'));
+};
+
+export const getGimbal = () => {
+    return getFetch(makeUrl('/gimbal'));
+};
+
+export const getRoundType = () => {
+    return getFetch(makeUrl('/round-type'));
+};
+
+export const getScanner = () => {
+    return getFetch(makeUrl('/scanner'));
+};
+
+export const getSteering = () => {
+    return getFetch(makeUrl('/steering'));
+};
+
+export const getThruster = () => {
+    return getFetch(makeUrl('/thruster'));
+};
+
+export const makeUrl = (endpoint, params = false, withBase = true) => {
+    const url = withBase ? baseURL : '';
+    params = params ? '?' + createParams(params) : '';
+    return url + endpoint + params;
+};
+
+const listValues = (params, key) =>
+    Array.isArray(params[key]) ? params[key].join(',') : params[key];
+
+const createParams = (params) =>
+    Object.keys(params)
+        .map(key => key + '=' + listValues(params, key))
+        .join('&');
+
+const getFetch = function(url) {
+    return fetch(url, {
+        method: 'get',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+        },
+    });
+};
+
+const deleteFetch = function(url) {
+    return fetch(url, {
+        method: 'delete',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+        },
+    });
+};
+
+const postFetch = function(url, data) {
+    return fetch(url, {
+        method: 'post',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+};
+
+const putFetch = function(url, data) {
+    return fetch(url, {
+        method: 'put',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/app';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 ReactDOM.render(
-    <App/>,
+    <Fragment>
+        <CssBaseline/>
+        <MuiThemeProvider theme={theme}>
+            <App/>
+        </MuiThemeProvider>
+    </Fragment>,
     document.getElementById('root'),
 );
